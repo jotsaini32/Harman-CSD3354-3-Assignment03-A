977640752514e90d3e68349028ef1f5f453bc1d3
visualstudio <visualstudio@122-PC36>
2019-03-15 20:39:22 -0400

    Bank Account Test

---
+++ Bank test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Bank test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Bank test")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6eb1583f-47b4-427a-bf53-95478f213cc1")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Bank test/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// Manvir Singh C0730069
+// Harmanjot Singh C0727172
+namespace Bank_test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        //Arrange
+
+        double beginningBalance = 11.99;
+
+        double debitAmount = 4.55;
+
+        double expected = 7.44;
+
+        BankAccount account = new BankAccount("Mr. Manvir Singh \nMr. Harmanjot Singh", beginningBalance);
+
+
+
+        //Act
+
+        account.Debit(debitAmount);
+
+
+
+            //Assert
+
+            double actual = account.Balance;
+
+        Assert.AreEqual(expected, actual, 0.0001, "Account nt debited correctly");
+
+    }
+}
